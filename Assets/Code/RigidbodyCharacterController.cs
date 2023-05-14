5a802685488f5652740a81a88252a09ccc508721
Szyszzz <jakzych@gmail.com>
2023-05-14 22:08:24 +0200

    Added Ground Check

--- Assets/Code/RigidbodyCharacterController.cs
+++ Assets/Code/RigidbodyCharacterController.cs
@@ -9,8 +9,12 @@ public class RigidbodyCharacterController : MonoBehaviour
 {
     [Header("Objects And Components")]
     [SerializeField] private GameObject ViewObject;
-    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private CapsuleCollider col;
+    [SerializeField] private GameObject middle;
 
+    [Header("Layers")]
+    [SerializeField] private LayerMask GroundLayer;
 
     [Header("View Settings")]
     [SerializeField] private float Deadzone;
@@ -22,6 +26,8 @@ public class RigidbodyCharacterController : MonoBehaviour
     [Header("Move Settings")]
     [SerializeField] private float MoveSpeed;
     [SerializeField] private float JumpForce = 1f;
+    [SerializeField] private float PlayerDrag = 4f;
+    [SerializeField] private float AirSpeedMultiplier = 0.5f;
 
     [Header("Rotation Info")]
     [SerializeField] private float VerticalAngle;
@@ -29,32 +35,43 @@ public class RigidbodyCharacterController : MonoBehaviour
 
     [Header("Move Info")]
     [SerializeField] private Vector2 MoveVectorInput;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float GroundedTolerance;
+    [SerializeField] private float MaxRayCastLength;
 
     PlayerInputActions playerInputActions;
     private float sensitivity_Vert;
     private float sensitivity_Hor;
     private Vector2 lastInput;
+    private float jumpStrength;
+    private float moveSpeed;
 
 
     private void Awake()
     {
+        
         Cursor.lockState = CursorLockMode.Locked;
 
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
         playerInputActions.Player.Jump.performed += PerformJump;
 
-        if (rigidbody == null)
+        if (rb == null)
         {
-            rigidbody = gameObject.GetComponent<Rigidbody>();
+            rb = gameObject.GetComponent<Rigidbody>();
         }
 
         if (ViewObject == null)
             Debug.LogError("ViewObject not present.");
+
+        MaxRayCastLength = ((CapsuleCollider)col).height / 2 + GroundedTolerance;
+        moveSpeed = MoveSpeed;
     }
 
     private void FixedUpdate()
     {
+        CheckIfGrounded();
+        Body_ChangeDrag();
         PerformMove();
     }
 
@@ -62,6 +79,11 @@ public class RigidbodyCharacterController : MonoBehaviour
     {
         MoveVectorInput = CalculateMoveInputs();
         PerformRotations();
+
+        if(playerInputActions.Player.Jump.IsPressed())
+        {
+            Invoke(nameof(PerformJump), 1f);
+        }
     }
 
     private void PerformRotations()
@@ -81,14 +103,22 @@ public class RigidbodyCharacterController : MonoBehaviour
 
     private void PerformJump(InputAction.CallbackContext cont)
     {
-        Debug.Log("Jump");
-        rigidbody.AddForce(JumpForce * Vector3.up, ForceMode.Impulse);
+        if(isGrounded)
+        { 
+            rb.AddForce(CalculateJumpStrength(), ForceMode.Impulse);
+        }
     }
 
     private Vector2 CalculateMoveInputs()
     {
         Vector2 input = playerInputActions.Player.Move.ReadValue<Vector2>() * MoveSpeed;
         return input;
+    }
+
+    private Vector3 CalculateJumpStrength()
+    {
+        jumpStrength = JumpForce * rb.mass * 2;
+        return jumpStrength * Vector3.up;
     }
 
     private Vector2 SmoothInput(Vector2 input)
@@ -104,6 +134,11 @@ public class RigidbodyCharacterController : MonoBehaviour
 
         lastInput = input;
         return new Vector2(x, y);
+    }
+
+    private void CheckIfGrounded()
+    {
+        isGrounded = Physics.Raycast(middle.transform.position, Vector3.down, col.height / 2 + GroundedTolerance);
     }
 
     private void View_VertRotation(Vector2 input)
@@ -129,6 +164,20 @@ public class RigidbodyCharacterController : MonoBehaviour
     private void Body_MovePlayer(Vector2 input)
     {
         Vector3 input3 = new Vector3(input.x,0,input.y);
-        rigidbody.AddForce(transform.TransformVector(input3), ForceMode.Force);
+        rb.AddForce(transform.TransformVector(input3), ForceMode.Force);
+    }
+
+    private void Body_ChangeDrag()
+    {
+        if (!isGrounded)
+        {
+            MoveSpeed = moveSpeed * AirSpeedMultiplier;
+            rb.drag = 0f;
+        }
+        else
+        {
+            MoveSpeed = moveSpeed;
+            rb.drag = PlayerDrag;
+        }
     }
 }
